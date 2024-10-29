7a8c6002884331879b48cdd4692820a06defaa88
DPRAHUL <dpranavr2021@gmail.com>
2024-10-29 23:35:55 +0530

    Implementation of liveblocks done , next is HomePage

--- docable/app/(root)/documents/[id]/page.tsx
+++ docable/app/(root)/documents/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
-import React from 'react'
+
 
 const Documents = () => {
   return (
